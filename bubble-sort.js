98c5b8c663f303d84788b31f301748a43fe2bdd3
Daniel Prada <daniel@lamusica.com>
2018-07-13 10:56:34 -0400

    Insertion sort

--- bubble-sort.js
+++ bubble-sort.js
@@ -1,36 +1,32 @@
 const chai = require("chai");
-const _ = require("underscore");
 const assert = chai.assert;
 const expect = chai.expect;
-
 chai.use(require("chai-sorted"));
+const _ = require("underscore");
 
 /*
- * Insertion Sort
+ * Bubble Sort
  */
 
 const array = [4,5,14,14,21,1,7,8,92,421,33,44,29,87,41,44];
-
-// For logging
 const original = array.slice();
 const lengthBefore = array.length;
+
 let operationCount = 0;
 
 for (let i = 0; i < array.length; i++) {
-
-    var iData = array[i];
-    var j = i - 1;
-
-    while (j >= 0 && array[j] > iData) {
-        array[j+1] = array[j];
-        j--;
+    for (let j = i + 1; j < array.length; j++) {
+        var iData = array[i];
+        var jData = array[j];
+        if (array[j] < array[i]) {
+            array[i] = jData;
+            array[j] = iData;
+        }
         operationCount++;
     }
-
-    array[j + 1] = iData;
 }
 
 assert.equal(lengthBefore, array.length);
-assert.equal(_.difference(array, original).length, 0);
 expect(array).to.be.sorted();
 console.log("Operation count", operationCount);
+assert.equal(_.difference(array, original).length, 0);
